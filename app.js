f462a95d0b7add9e585c10cb4019cc2a40a2b6bd
MicMac <miclet002@gmail.com>
2019-11-24 15:58:49 +0100

    pages videos consultable

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 var moment = require('moment');
 var session = require('cookie-session');
-const { searchVideos } = require("@micmac/youtube");
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 var http = require('http');
 var fs = require('fs');
+var myYT = require('@micmac/youtube');
+var SearchVideos = myYT.SearchVideos;
+var SearchThisVideo = myYT.SearchThisVideo;
+var searchVideos = new SearchVideos();
+var searchThisVideo = new SearchThisVideo();
+
+
+
 
 // Chargement du fichier index.html affiché au client
 
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-
 //api fortnite
 //const fortniteapi = require('fortnite-api-js');
 //
@@ -31,41 +37,25 @@ var io = require('socket.io').listen(server);
 // use res.render to load up an ejs view file
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-
-
 console.log('8080 is the magic port');
-app.use(session({secret: 'todotopsecret'}))
-        //console.log(__dirname);
-        .use('/public', express.static('public'))
+app.use(session({secret: 'todotopsecret'}));
+//console.log(__dirname);
+app.use('/public', express.static('public'))
         // index page 
         .get('/', function (req, res) {
             var params = {
                 css: 'global.css',
                 video: searchVideos
             };
-            console.log (params.video);
             res.render('pages/index.ejs', {
                 params: params
             });
         })
         .get('/video', function (req, res) {
-            var video = {
-                url: "https://www.youtube.com/embed/6eiDDZEWQYE?ecver=1&amp;autoplay=1&amp;iv_load_policy=1&amp;yt:stretch=4:3&amp;autohide=1&amp;color=red&amp;width=800&amp;width=400",
-                game: "simcity",
-                article: {
-                    titre: "Je suis le maire de YouTube-City !!",
-                    paragraphe1: "Quand je ne suis pas en train de faire un top 1 sur Fortnite, je gère les préoccupations de millions de gens, dur ma vie de star.",
-                    paragraphe2: ""
-                }
-            };
-            var params = {
-                css: 'videoPage1.css'
-            };
-            res.render('/videoActivity', {
-                video: video,
-                params: params
-            });
+                    searchThisVideo.goToVideoPage(searchVideos.alexVideos[req.query.no].idVideo, 'global.css', res,'pages/videoActivity.ejs');               
+                        
         })
+
 // about page 
         .get('/test', function (req, res) {
             console.log('test');
@@ -77,29 +67,26 @@ app.use(session({secret: 'todotopsecret'}))
                 params: params
             });
         });
-       
-   io.sockets.on('connection', function(socket) {
-    socket.emit('message', { content: 'Vous êtes bien connecté !', importance: '1' });
-    socket.on('nouveauClient', function() {
-           
-            console.log("nouveau client");
-        })
-        .on('connexionIndex', function() {
-            socket.emit('identification');
-            socket.broadcast.emit('entree');
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', {content: 'Vous êtes bien connecté !', importance: '1'});
+    socket.on('nouveauClient', function () {
 
-        })
-        .on('message', function(message, noOfSouth) {
-           
-        })
-        .on('move', function(_leftF, _topF, _time, _anim) {
-           
-        })
-        .on('disconnect', function(reason) {
-            console.log(reason);
+        console.log("nouveau client");
+    })
+            .on('connexionIndex', function () {
+                socket.emit('identification');
+                socket.broadcast.emit('entree');
+            })
+            .on('message', function (message, noOfSouth) {
 
-            // clients.splice(i, 1);
-            // clientsInfo.splice(i, 1);
-        });
-    });
-    server.listen(8080);
+            })
+            .on('move', function (_leftF, _topF, _time, _anim) {
+
+            })
+            .on('disconnect', function (reason) {
+                console.log(reason);
+                // clients.splice(i, 1);
+                // clientsInfo.splice(i, 1);
+            });
+});
+server.listen(8080);
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,12 +3,33 @@ window.onload = function () {
     hBgd = 1080;
     hScreen = window.innerHeight;
     wScreen = window.innerWidth;
-    document.body.style.height= hScreen + 'px';
-    document.body.style.width= wScreen + 'px';
+    document.body.style.height = hScreen + 'px';
+    document.body.style.width = wScreen + 'px';
     $('.news').toggleClass('newsStart');
-    
-        
-     
+    var allVideos = JSON.parse($('#hidden').text());
+    console.log(JSON.stringify(allVideos));
+    var videoCard = new Object();
+    var newNode = new Object();
+    var masterCard = document.getElementById('masterCard');
+    for (let i = 0; i < 3; i++) {
+        videoCard[i] = document.createElement("A");
+        videoCard[i].innerHTML = '<div class="card-body pb-0">'
+                + '                  <h5 class="card-title">' + allVideos.alexVideos[i].title + '</h5>'
+                + '                  <p class="card-text">' + allVideos.alexVideos[i].description + '</p>'
+                + '              </div>'
+                + '              <div class="container row">'
+                + '                  <img id="thumbnail_' + i + '" class="card-img-top col-md-6" src="' + allVideos.alexVideos[i].urlThumbnails + '" alt="' + allVideos.alexVideos[i].title + '">'
+                + '                  <p class="card-text text2 col-md-6 pt-2">' + allVideos.alexVideos[i].description2 + '</p>'
+                + '              </div>'
+                + '              <div class="card-footer">'
+                + '                  <small>Posté le ' + allVideos.alexVideos[i].date + '</small>'
+                + '              </div>';
+        videoCard[i].setAttribute("href", "/video?no=" + i);
+        videoCard[i].classList.add("card");
+        masterCard.appendChild(videoCard[i]);
+    }
+    ;
+// '<a href="\video?no=' + i + '" class="card">'
 //    setTimeout(function(){
 //        $('.news').toggleClass('newsStart');
 //    },100000);
