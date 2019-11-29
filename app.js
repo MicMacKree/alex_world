a994f4bdc2dfe79db27c48b44196920891f59e87
MicMac <miclet002@gmail.com>
2019-11-29 23:19:30 +0100

    enregistrement et lecture des infos de l'api youtube en format json

--- app.js
+++ app.js
@@ -12,8 +12,8 @@
  var app = express ();
  app.set ('view engine', 'ejs');
  var http = require ('http');
- var fs = require ('fs');
-
+ const fs = require ('fs');
+ 
 //api fortnite
 //const fortniteapi = require('fortnite-api-js');
 //
@@ -28,23 +28,8 @@
 //});
 // fonction pour consulter les infos des videos, à rafraichir a chque nouvelle entrée sur la chaine
 
- var getVideos = function () {
-     var myYT = require ('@micmac/youtube');
-//     var SearchVideos = myYT.SearchVideos;
-//     var SearchThisVideo = myYT.SearchThisVideo;
-     var getAllVideos = myYT.getAllVideosInfos;
-//     var searchVideos = new SearchVideos ();
-//     var searchThisVideo = new SearchThisVideo ();
-     getAllVideos ().then (function (response) {
-         console.log (response);
-         let videosDatas = JSON.stringify (response);
-         fs.writeFileSync ('video.json', videosDatas);
-     });
- };
- //getVideos();
- 
- //provisoirement stocké ici en attendant mieux, les JSON des vignettes manga, culture ....
- 
+//provisoirement stocké ici en attendant mieux, les JSON des vignettes manga, culture ....
+
  var titan = {
      rep: 'titan',
      title: 'L\'attaque des titans',
@@ -54,71 +39,162 @@
          pitch: 'L’histoire tourne autour du personnage d’Eren Jäger dans un monde où l’humanité vit entourée d’immenses murs pour se protéger de créatures gigantesques, les Titans. Le récit raconte le combat mené par l’humanité pour reconquérir son territoire, en éclaircissant les mystères dus à l’apparition des Titans. '
      }
  };
+
+// récupère les infos au format json de l'api youtube data puis les écrits dans un fichier public/src/video.json
+
+ var getApiInfoThenWrite = function () {
+     console.log ('entree dans getApiInfoThenWrites()');
+     return new Promise (function (resolve, reject) {
+         var myYT = require ('@micmac/youtube');
+         var getAllVideos = myYT.getAllVideosInfos;
+         getAllVideos ().catch (function (error) {
+             console.log ('erreur loading youtube info');
+             reject (error);
+         }).then (function (response) {
+             console.log ('loading youtube info : ');
+             var dataString = JSON.stringify (response);
+             var dataJSON = response;
+             fs.writeFile ('public/src/video.json', dataString, function (error) {
+                 if (error)
+                     {
+                         console.log ('écriture erreur');
+                         reject (error);
+                     }
+                 else
+                     {
+                         console.log ('write File OK !');
+                         resolve (dataJSON);
+                     }
+             });
+         });
+     }, function (error, response) {
+         if (error)
+             {
+                 reject (error);
+             }
+         if (response)
+             {
+                 resolve (response);
+             }
+     });
+ };
+
+// fonction lecture du fichier public/src/video.json 
+
+ readVideoJSON = function () {
+     return new Promise (function (resolve, reject) {
+         fs.readFile ('public/src/video.json', 'utf8', function (err, data) {
+             if (err)
+                 {
+                     console.log ('erreur de lecture');
+                     console.log (err);
+                     reject (err);
+                 }
+             if (data)
+                 {
+                     console.log ('lecture OK');
+                     console.log (data);
+                     resolve (JSON.parse (data));
+                 }
+         });
+     });
+ };
+
  
+
+
+
+
+// fonction appel de getAndWrite() puis renvoie la lecture de public/src/video.json sous forme de json. 
+
+ var readApiThenRecordThenRead = function () {
+     getApiInfoThenWrite ().then (function (response) {
+         if (response)
+             {
+                 console.log ('ecriture ok!');
+                 readVideoJSON ().then (function (response) {
+                     console.log ('lecture en sortie ok');
+                     console.log (response);
+                     return response;
+                 });
+             }
+     }).catch (function (error) {
+         console.log ('error capté en sortie :');
+         console.log (error);
+     });
+ };
  
- // Démarrage du serveur
+ readVideoJSON().then(function(response) {
+     serverStart(response);
+ });
  
- var server = http.createServer (app);
- var io = require ('socket.io').listen (server);
+ // fonction démarrage du serveur
+ var serverStart = function (videoData) {
+     var server = http.createServer (app);
+     var io = require ('socket.io').listen (server);
 
 // use res.render to load up an ejs view file
 
- console.log (moment ().format ('MMMM Do YYYY, h:mm:ss a'));
- console.log ( process.env.PORT + 'is the magic port');
- app.use (session ({
-     secret: 'todotopsecret'
- }));
-//console.log(__dirname);
- app.use ('/public', express.static ('public'))
-         // index page 
-         .get ('/', function (req, res) {
-             var params = {
-                 css: 'global.css',
-                 carroussel: titan,
-                 video: searchVideos
-             };
-             res.render ('pages/index.ejs', {
-                 params: params
-             });
-         })
-         .get ('/video', function (req, res) {
-             searchThisVideo.goToVideoPage (searchVideos.allVideos[req.query.no].idVideo, 'global.css', res, 'pages/videoActivity.ejs');
-         })
+     console.log (moment ().format ('MMMM Do YYYY, h:mm:ss a'));
+     console.log (process.env.PORT + 'is the magic port');
+     app.use (session ({
+         secret: 'todotopsecret'
+     }));
+     app.use ('/public', express.static ('public'))
+             // index page 
+             .get ('/', function (req, res) {
+                 var params = {
+                     css: 'global.css',
+                     carroussel: titan,
+                     video: videoData
+                 };
+                 res.render ('pages/index.ejs', {
+                     params: params
+                 });
+             })
+             .get ('/video', function (req, res) {
+                 searchThisVideo.goToVideoPage (searchVideos.allVideos[req.query.no].idVideo, 'global.css', res, 'pages/videoActivity.ejs');
+             })
 
 // about page 
-         .get ('/test', function (req, res) {
-             console.log ('test');
-             youtube.searchVideos ();
-             var params = {
-                 css: 'videoPage1.css'
-             };
-             res.render ('pages/test.ejs', {
-                 params: params
+             .get ('/test', function (req, res) {
+                 console.log ('test');
+                 youtube.searchVideos ();
+                 var params = {
+                     css: 'videoPage1.css'
+                 };
+                 res.render ('pages/test.ejs', {
+                     params: params
+                 });
              });
+     io.sockets.on ('connection', function (socket) {
+         socket.emit ('message', {
+             content: 'Vous êtes bien connecté !',
+             importance: '1'
          });
- io.sockets.on ('connection', function (socket) {
-     socket.emit ('message', {
-         content: 'Vous êtes bien connecté !',
-         importance: '1'
+         socket.on ('nouveauClient', function () {
+
+             console.log ("nouveau client");
+         })
+                 .on ('connexionIndex', function () {
+                     socket.emit ('identification');
+                     socket.broadcast.emit ('entree');
+                 })
+                 .on ('message', function (message, noOfSouth) {
+
+                 })
+                 .on ('move', function (_leftF, _topF, _time, _anim) {
+
+                 })
+                 .on ('disconnect', function (reason) {
+                     console.log (reason);
+                     // clients.splice(i, 1);
+                     // clientsInfo.splice(i, 1);
+                 });
      });
-     socket.on ('nouveauClient', function () {
+     server.listen (process.env.PORT);
+ };
 
-         console.log ("nouveau client");
-     })
-             .on ('connexionIndex', function () {
-                 socket.emit ('identification');
-                 socket.broadcast.emit ('entree');
-             })
-             .on ('message', function (message, noOfSouth) {
 
-             })
-             .on ('move', function (_leftF, _topF, _time, _anim) {
 
-             })
-             .on ('disconnect', function (reason) {
-                 console.log (reason);
-                 // clients.splice(i, 1);
-                 // clientsInfo.splice(i, 1);
-             });
- });
- server.listen (8080);
+    
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,33 +1,51 @@
-window.onload = function () {
-    wBgd = 1920;
-    hBgd = 1080;
-    hScreen = window.innerHeight;
-    wScreen = window.innerWidth;
-    document.body.style.height = hScreen + 'px';
-    document.body.style.width = wScreen + 'px';
-    $('.news').toggleClass('newsStart');
-    var allVideos = JSON.parse($('#hidden').text());
-    console.log(JSON.stringify(allVideos));
-    var videoCard = new Object();
-    var masterCard = document.getElementById('masterCard');
-    for (let i = 0; i < 3; i++) {
-        videoCard[i] = document.createElement("A");
-        videoCard[i].innerHTML = '<div class="card-body pb-0">'
-                + '                  <h5 class="card-title">' + allVideos.allVideos[i].title + '</h5>'
-                + '                  <p class="card-text">' + allVideos.allVideos[i].description + '</p>'
-                + '              </div>'
-                + '              <div class="container row">'
-                + '                  <img id="thumbnail_' + i + '" class="card-img-top col-md-6" src="' + allVideos.allVideos[i].urlThumbnails + '" alt="' + allVideos.allVideos[i].title + '">'
-                + '                  <p class="card-text text2 col-md-6 pt-2">' + allVideos.allVideos[i].description2 + '</p>'
-                + '              </div>'
-                + '              <div class="card-footer">'
-                + '                  <small>Posté le ' + allVideos.allVideos[i].date + '</small>'
-                + '              </div>';
-        videoCard[i].setAttribute("href", "/video?no=" + i);
-        videoCard[i].classList.add("card");
-        masterCard.appendChild(videoCard[i]);
-    }
-    ;
+ window.onload = function () {
+     wBgd = 1920;
+     hBgd = 1080;
+     hScreen = window.innerHeight;
+     wScreen = window.innerWidth;
+     document.body.style.height = hScreen + 'px';
+     document.body.style.width = wScreen + 'px';
+     $ ('.news').toggleClass ('newsStart');
+     var allVideos = JSON.parse ($ ('#hidden').text ());
+     console.log (JSON.stringify (allVideos));
+     var videoCard = new Object ();
+     var masterCard_0 = document.getElementById ('masterCard_0');
+     var masterCard_1 = document.getElementById ('masterCard_1');
+     var masterCard_2 = document.getElementById ('masterCard_2');
+     var masterCard_3 = document.getElementById ('masterCard_3');
+     for (let i = 0;
+             i < 10;
+             i ++)
+         {
+             videoCard[i] = document.createElement ("A");
+             videoCard[i].innerHTML = '<div class="card-body pb-0">'
+                     + '                  <h5 class="card-title">' + allVideos.video[i].title + '</h5>'
+                     + '                  <p class="card-text">' + allVideos.video[i].description2+ ' ... ' + '</p>'
+                     + '              </div>'
+                     + '              <div class="container row">'
+                     + '                  <img id="thumbnail_' + i + '" class="card-img-top col-md-6" src="' + allVideos.video[i].thumbnail + '" alt="' + allVideos.video[i].title + '">'
+                     + '                  <p class="card-text text2 col-md-6 pt-2"></p>'
+                     + '              </div>'
+                     + '              <div class="card-footer">'
+                     + '                  <small>Posté le ' + allVideos.video[i].date + '</small>'
+                     + '              </div>';
+             videoCard[i].setAttribute ("href", "/video?no=" + i);
+             videoCard[i].classList.add ("card");
+         }
+     ;
+     masterCard_0.appendChild (videoCard[0]);
+     masterCard_0.appendChild (videoCard[1]);
+     masterCard_0.appendChild (videoCard[2]);
+     masterCard_1.prepend (videoCard[3]);
+     masterCard_1.appendChild (videoCard[4]);
+     masterCard_2.appendChild (videoCard[5]);
+     masterCard_2.appendChild (videoCard[6]);
+     masterCard_2.appendChild (videoCard[7]);
+     masterCard_3.appendChild (videoCard[8]);
+     masterCard_3.appendChild (videoCard[9]);
+ };
+
+
 // '<a href="\video?no=' + i + '" class="card">'
 //    setTimeout(function(){
 //        $('.news').toggleClass('newsStart');
@@ -49,6 +67,6 @@ window.onload = function () {
 //    setTimeout(function () { 
 //        portable.classList.toggle('zoom');
 //    }, 100);
-};
+
 
 
